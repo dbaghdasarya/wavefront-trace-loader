0b03aa8341e98678ea784a91640eec401b3af039
Sirak Ghazaryan <sirakghazaryan@gmail.com>
2019-11-05 18:50:10 +0400

    MONIT-16332: Provide ability to setup distributions of spans durations inside of one trace type. (#5)

--- src/main/java/com/wavefront/helpers/TraceTypePatternSanitizer.java
+++ src/main/java/com/wavefront/helpers/TraceTypePatternSanitizer.java
@@ -3,22 +3,39 @@ package com.wavefront.helpers;
 import com.fasterxml.jackson.databind.util.StdConverter;
 import com.wavefront.SpanSender;
 import com.wavefront.TraceTypePattern;
+import com.wavefront.TraceTypePattern.TagVariation;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.logging.Logger;
 
+import static com.wavefront.helpers.Defaults.DEFAULT_MANDATORY_TAGS;
+import static com.wavefront.helpers.Defaults.DEFAULT_NESTING_LEVEL;
+import static com.wavefront.helpers.Defaults.DEFAULT_SPANS_DISTRIBUTIONS;
+import static com.wavefront.helpers.Defaults.DEFAULT_TRACE_DURATIONS;
+import static com.wavefront.helpers.Defaults.DEFAULT_TYPE_NAME_PREFIX;
+
+/**
+ * Sanitizer class which converts incorrect input values to default ones.
+ *
+ * @author Davit Baghdasaryan (dbaghdasarya@vmware.com)
+ */
 public class TraceTypePatternSanitizer extends StdConverter<TraceTypePattern, TraceTypePattern> {
   private static final Logger LOGGER = Logger.getLogger(SpanSender.class.getCanonicalName());
-  private final static List<TraceTypePattern.TagVariation> DEFAULT_MANDATORY_TAGS =
-      List.of(new TraceTypePattern.TagVariation("application",
-              List.of("Application_1")),
-          new TraceTypePattern.TagVariation("service",
-              List.of("Service_1", "Service_2")));
-
+  private static int currentTypeIndex = 1;
 
   @Override
   public TraceTypePattern convert(TraceTypePattern value) {
+    // check trace type name
+    if (value.traceTypeName == null || value.traceTypeName.isEmpty()) {
+      value.traceTypeName = DEFAULT_TYPE_NAME_PREFIX + currentTypeIndex++;
+      LOGGER.warning("Incorrect value of traceTypeName was replaced by " + value.traceTypeName);
+    }
+    // check nesting level
+    if (value.nestingLevel <= 0) {
+      value.nestingLevel = DEFAULT_NESTING_LEVEL;
+      LOGGER.warning("Incorrect value for nestingLevel was replaced by " + value.nestingLevel);
+    }
+
     // Check and fix the tags set
     // Mandatory Tags must exist in the set and must have values
     if (value.mandatoryTags == null) {
@@ -26,7 +43,7 @@ public class TraceTypePatternSanitizer extends StdConverter<TraceTypePattern, Tr
     }
 
     DEFAULT_MANDATORY_TAGS.forEach(tagVariation -> {
-          TraceTypePattern.TagVariation tempTagVariation = value.mandatoryTags.stream().
+          TagVariation tempTagVariation = value.mandatoryTags.stream().
               filter(givenTag -> givenTag.tagName.equals(tagVariation.tagName)).findFirst().
               orElseGet(() -> {
                 value.mandatoryTags.add(tagVariation);
@@ -46,6 +63,34 @@ public class TraceTypePatternSanitizer extends StdConverter<TraceTypePattern, Tr
       value.optionalTagsPercentage = 100;
       LOGGER.warning("Meaningless value of the optionalTagsPercentage was replaced with 100%");
     }
+
+    // check traceDurations and spansDurations. As traceDurations have priority over spansDurations
+    // the spansDurations will be skipped if traceDurations is set
+    if (value.traceDurations == null) {
+      value.traceDurations = new ArrayList<>();
+    }
+    if (value.spansDurations == null) {
+      value.spansDurations = new ArrayList<>();
+    }
+    if (value.traceDurations.isEmpty() && value.spansDurations.isEmpty()) {
+      value.traceDurations.addAll(DEFAULT_TRACE_DURATIONS);
+      LOGGER.warning("Neither traceDurations nor spansDurations were provided. traceDurations " +
+          "were defaulted to " + value.traceDurations.toString());
+    }
+    if (!value.traceDurations.isEmpty() && !value.spansDurations.isEmpty()) {
+      LOGGER.warning("spansDurations will be skipped, as traceDurations is set.");
+    }
+
+    // spans distributions
+    if (value.spansDistributions == null) {
+      value.spansDistributions = new ArrayList<>();
+    }
+    if (value.spansDistributions.isEmpty()) {
+      value.spansDistributions.addAll(DEFAULT_SPANS_DISTRIBUTIONS);
+      LOGGER.warning("spansDistributions were not provided. Defaulted to " +
+          value.spansDistributions.toString());
+    }
+
     return value;
   }
 }
