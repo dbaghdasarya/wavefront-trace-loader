200df1f5c1f67a7250e88bcdafc1e81d2490923e
Sirak Ghazaryan <sirakghazaryan@gmail.com>
2019-11-21 18:11:55 +0400

    MONIT-16853: Added stat info about errors, fixed issue related errorRates (#9)

--- src/main/java/com/wavefront/helpers/Statistics.java
+++ src/main/java/com/wavefront/helpers/Statistics.java
@@ -17,14 +17,18 @@ import java.util.stream.Collectors;
  */
 public class Statistics {
   private int tracesSum = 0;
+  private int errorsSum = 0;
   private final Map<String, TypeStatistic> tracesByType = new HashMap<>();
 
-  public void offer(String traceTypeName, List<List<Span>> trace, int traceDuration) {
+  public void offer(String traceTypeName, List<List<Span>> trace, int traceDuration, boolean error) {
     tracesSum++;
+    if (error) {
+      errorsSum++;
+    }
     int spansCount = trace.stream().flatMap(List::stream).collect(Collectors.toList()).size();
     tracesByType.putIfAbsent(traceTypeName, new TypeStatistic());
     tracesByType.computeIfPresent(traceTypeName, (k, v) -> {
-      v.update(spansCount, traceDuration);
+      v.update(spansCount, traceDuration, error);
       return v;
     });
   }
@@ -33,6 +37,8 @@ public class Statistics {
   public String toString() {
     StringBuilder sb = new StringBuilder();
     sb.append("Total traces: ").append(tracesSum).append('\n');
+    sb.append("Total errors: ").append(errorsSum).append('\n');
+    sb.append("Total errors percentage: ").append(Math.round((double) errorsSum / tracesSum * 100)).append('\n');
     tracesByType.forEach((k, v) -> {
       sb.append("\nType: ").append(k).append('\n');
       sb.append("Count ").append(v.count).append('\n');
@@ -45,6 +51,9 @@ public class Statistics {
           append(Math.round((double) v.traceDuration / v.count)).append('\n');
       sb.append("Trace duration min ").append(v.traceDurationMin).append('\n');
       sb.append("Trace duration max ").append(v.traceDurationMax).append('\n');
+      sb.append("Errors count ").append(v.errorCount).append('\n');
+      sb.append("Errors percentage ").
+          append(Math.round((double) v.errorCount / errorsSum * 100)).append('\n');
     });
     return sb.toString();
   }
@@ -53,6 +62,8 @@ public class Statistics {
     ObjectMapper mapper = new ObjectMapper();
     ObjectNode rootNode = mapper.createObjectNode();
     rootNode.put("Total traces", tracesSum);
+    rootNode.put("Total errors", errorsSum);
+    rootNode.put("Total errors percentage", Math.round((double) errorsSum / tracesSum * 100));
     tracesByType.forEach((k, v) -> {
       ObjectNode node = mapper.createObjectNode();
       node.put("Count", v.count);
@@ -63,6 +74,8 @@ public class Statistics {
       node.put("Trace duration mean", Math.round((double) v.traceDuration / v.count));
       node.put("Trace duration min", v.traceDurationMin);
       node.put("Trace duration max", v.traceDurationMax);
+      node.put("Errors count", v.errorCount);
+      node.put("Errors percentage", Math.round((double) v.errorCount / errorsSum * 100));
       rootNode.set(k, node);
     });
     return mapper.writerWithDefaultPrettyPrinter().writeValueAsString(rootNode);
@@ -71,6 +84,7 @@ public class Statistics {
 
   private static class TypeStatistic {
     int count = 0;
+    int errorCount = 0;
     int spansSum = 0;
     int spansMin = Integer.MAX_VALUE;
     int spansMax = Integer.MIN_VALUE;
@@ -78,7 +92,10 @@ public class Statistics {
     int traceDurationMin = Integer.MAX_VALUE;
     int traceDurationMax = Integer.MIN_VALUE;
 
-    void update(int spansCount, int duration) {
+    void update(int spansCount, int duration, boolean error) {
+      if (error) {
+        errorCount++;
+      }
       count++;
       spansSum += spansCount;
       spansMin = Math.min(spansMin, spansCount);
